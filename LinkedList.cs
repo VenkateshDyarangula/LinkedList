b4f5a58a01fd6e2f8cbc4211f7345aa1ed01e6e4
venkatesh179 <venkateshdyarangula@gmail.com>
2021-04-16 19:04:26 +0530

    UC1_Simple_LinkedList

---
+++ LinkedList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LinkedList_all_
+{
+    class LinkedList
+    {
+        internal Node head;
+        public static void Main(string[] args)
+        { 
+            LinkedList list = new LinkedList();
+            list.head = new Node(56);
+            Node second = new Node(30);
+            Node third = new Node(70);
+            list.head.next = second;
+            second.next = third;
+            Console.WriteLine(list.head.data);
+            Console.WriteLine(second.data);
+            Console.WriteLine(third.data);
+        }
+    }
+}
