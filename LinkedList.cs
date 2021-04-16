923bf6795f6f14a610627b5b436a4164c0c2db03
venkatesh179 <venkateshdyarangula@gmail.com>
2021-04-16 19:13:13 +0530

    UC3 Add

--- LinkedList.cs
+++ LinkedList.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 
-namespace UC2_AddToLinkedList
+namespace UC3_AddFirstPosition
 {
     class LinkedList
     {
+
+
         internal Node head;
         internal void Add(int data)
         {
@@ -21,14 +23,47 @@ namespace UC2_AddToLinkedList
             }
             Console.WriteLine("{0} inserted into the linked list", node.data);
         }
+        internal void AddAtFirst(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+                Console.WriteLine("{0} inserted into the linked list", node.data);
+            }
+            else
+            {
+                node.next = head;
+                head = node;
 
-        public static void Main(string[] args)
+                Console.WriteLine("{0} inserted into the linked list", node.data);
+            }
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next; //temp=null
+            }
+        }
+        static void Main(string[] args)
         {
             LinkedList list = new LinkedList();
 
-            list.Add(50);
-            list.Add(25);
-            list.Add(76);
+            list.Add(60);
+            list.Add(84);
+            list.Add(56);
+            list.AddAtFirst(94);
+            list.AddAtFirst(11);
+            list.AddAtFirst(46);
+            list.Display();
         }
     }
 }
--- Node.cs
+++ Node.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace UC2_AddToLinkedList
+namespace UC3_AddFirstPosition
 {
     public class Node
     {
